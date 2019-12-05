bdaf2a049a6e5ff8d1a20c8023b4d563515e63f4
Columbia91 <alpha.z91@mail.ru>
2019-12-05 21:38:36 +0600

    Добавьте файлы проекта.

---
+++ PatientCard/Models/History.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PatientCard.Models
+{
+    public class History
+    {
+        public int Id { get; set; }
+        public string Speciality { get; set; }
+        public string Doctor { get; set; }
+        public string Diagnose { get; set; }
+        public string Complaints { get; set; }
+        public DateTime VisitDate { get; set; }
+        public int PatientId { get; set; }
+        public Patient Patient { get; set; }
+    }
+}
+/*1. Заполнение личных данных (ИИН, ФИО, Адрес, Телефон)
+2. Заполнение истории посещений в табличном виде (Специалист (терапевт, лор, и т.д.), ФИО врача, диагноз, жалобы, дата посещения)
+3. Поиск пациента по ИИН и ФИО (в отдельном контроллере со своими Views)*/
---
+++ PatientCard/Models/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PatientCard.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        [StringLength(12, MinimumLength = 12, ErrorMessage = "ИИН состоит из 12 цифр")]
+        public int IIN { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
+/*1. Заполнение личных данных (ИИН, ФИО, Адрес, Телефон)
+2. Заполнение истории посещений в табличном виде (Специалист (терапевт, лор, и т.д.), ФИО врача, диагноз, жалобы, дата посещения)
+3. Поиск пациента по ИИН и ФИО (в отдельном контроллере со своими Views)*/
